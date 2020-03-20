d5d891985d9c1e8831058f7c46dbe66452e84ecb
Abhishek Garain <abhi211199@gmail.com>
2020-03-20 20:18:03 +0530

    added json editor

--- src/buttons.js
+++ src/buttons.js
@@ -2,23 +2,11 @@
 
 // Show an open file dialog, select a file, and load it into the editor
 function openFile(){
-  // dialog.showOpenDialog(
-  //   {filters: [{ name: 'json', extensions: ['json'] }]},
-	// function (fileName) {
-  //   if (fileName !== undefined)
-	// {
-  //   console.log(fileName);
-	//   // readFile(fileName[0]);
-	//   // openFileName = fileName[0];
-  // }
-  // });
   dialog.showOpenDialog(require('electron').remote.BrowserWindow, {
     properties: ['openFile', 'openDirectory']
   }).then(result => {
     readFile(result.filePaths[0])
     openFileName=result.filePaths[0];
-    // console.log(result.canceled)
-    // console.log(result.filePaths)
   }).catch(err => {
     console.log(err)
   })
@@ -32,7 +20,6 @@ function newFile(){
 
 // Helper function for saving a file
 function writeFile(fileName){
-  // alert("abhi")
   fs.writeFile(fileName, data, function (err) {
     if(err){
       alert("An error occurred creating the file " + err.message);
@@ -48,7 +35,6 @@ function saveFile() {
   parsedJson = editor.get();
   data = JSON.stringify(parsedJson, null, 2);
   console.log(data);
-  // writeFil("C:\abhi.json");
   if (openFileName == "")
   {
     dialog.showSaveDialog(require('electron').remote.BrowserWindow, {
@@ -79,19 +65,6 @@ function showJSON(){
   // console.log(JSON.parse(JSON.stringify(result,null,2)));
   document.getElementById("jsonViewer").innerHTML=JSON.stringify(editor.get(), null, 2);
 }
-
-// function cmd()
-// {
-//   const { exec } = require('child_process').remote;
-// exec('dir', (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.error(`stderr: ${stderr}`);
-// });
-// }
 
 // Add the event handlers to the buttons
 document.getElementById('btnOpenFile').addEventListener('click', openFile);
--- src/cfg_create.js
+++ src/cfg_create.js
@@ -12,10 +12,10 @@ function create()
         if(btns[i].checked==true)
         {
             s+="config="+btns[i].name+"\n";
-            // fs.copyFile('templates/variables.json', 'flint/'+btns[i].name, (err) => {
-            //     if (err) throw err;
-            //     console.log('source.txt was copied to destination.txt');
-            // });
+            fs.copyFile('templates/variables.json', 'flint/'+btns[i].name, (err) => {
+                if (err) throw err;
+                console.log('source.txt was copied to destination.txt');
+            });
         }
     }
     fs.writeFile('flint/gcbm_config.cfg', s, (err) => {
@@ -25,6 +25,87 @@ function create()
     console.log(s);
 }
 
+fs.access("flint/", fs.constants.F_OK, (err) => {
+    if(err)
+    {
+        document.getElementById("starter").style.display="block";
+        document.getElementById("checker").style.display="none";
+    }
+    else
+    {
+        document.getElementById("starter").style.display="none";
+        document.getElementById("checker").style.display="block";
+    }
+});
 
+function startover(){
+    fs.rmdir("flint/",{recursive:true},err=>{
+        if(err)
+        alert("can't delete directory");
+        else
+        {
+            document.getElementById("starter").style.display="block";
+            document.getElementById("checker").style.display="none";
+        }
+    });
+}
 
+function readdir()
+{
+    fs.readdir("flint/",(err,files)=>{
+        for(i=0;i<files.length;i++)
+        {
+            document.getElementById("panel").innerHTML+="<button name='"+files[i]+"' onClick='openjson(this.name)'>"+files[i]+"</button>"
+        }
+        console.log(files);
+    })
+}
+
+function openjson(imp)
+{
+    if(flag==1)
+    {
+        aler
+    }
+    readFile("flint/"+imp);
+    flag=1;
+    openFileName=imp;
+}
+
+function readFile(filepath){
+    fs.readFile(filepath, 'utf-8', function (err, data) {
+      if (err){
+        alert("An error occurred reading the file: " + err.message);
+        return;
+      }
+      console.log(data);
+      parsedJson = JSON.parse(data);
+      editor.set(parsedJson);
+    });
+}
+
+flag=0;
+
+function saveFile() {
+    parsedJson = editor.get();
+    data = JSON.stringify(parsedJson, null, 2);
+    console.log(data);
+    writeFile("flint/"+openFileName);
+    flag=0;
+}
+
+function writeFile(fileName){
+  fs.writeFile(fileName, data, function (err) {
+    if(err){
+      alert("An error occurred creating the file " + err.message);
+  }
+  else{
+    console.log("save");
+  }
+})
+}
+
+document.getElementById('startover').addEventListener('click',readdir);
 document.getElementById('create').addEventListener('click',create);
+document.getElementById('btnSaveFile').addEventListener('click', saveFile);
+var editor = new JSONEditor(document.getElementById("jsonEditor"), {});
