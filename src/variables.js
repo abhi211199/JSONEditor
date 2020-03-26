8fc812eb30cda9311ff91e9ebeac8b099f47c526
Abhishek Garain <abhi211199@gmail.com>
2020-03-27 00:34:19 +0530

    dynamic addition of transforms in variables.json

--- src/variables.js
+++ src/variables.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var editor=document.getElementById("editor");
 
+//adding base fields
 editor.innerHTML="<div class='container'><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i>"+
 "<div class='row'><div class='col-25'><label>enable_peatland:</label></div><div class='col-75'><input type='checkbox' name='enable_peatland'></div></div><br>"+
 "<div class='row'><div class='col-25'><label>enable_moss:</label></div><div class='col-75'><input type='checkbox' name='enable_moss'></div></div><br>"+
@@ -12,26 +13,25 @@ editor.innerHTML="<div class='container'><i class='fa fa-copy' style='font-size:
 "<div class='row'><div class='col-25'><label>age_maximum:</label></div><div class='col-75'><input type='text' name='age_maximum'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
 "<div class='row'><div class='col-25'><label>slow_ag_to_bg_mixing_rate:</label></div><div class='col-75'><input type='text' name='slow_ag_to_bg_mixing_rate'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div></div><br><br>";
 
+//adding transfoms
 function add_object()
 {
     for(i=0;i<document.getElementById("trno").value;i++)
     editor.innerHTML+="<div class='container'><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i>"+
    "<div class='transformdiv'><div class='container'><div class='row'><div class='col-25'><label>Name of the transform:</label></div><div class='col-75'><input type='text' name='inpi2'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>Type of transform:</label></div><div class='col-75'><select id='transforms'><option value='LocationIdxFromFlintDataTransform'>LocationIdxFromFlintDataTransform</option><option value='SQLQueryTransform'>SQLQueryTransform</option><option value='CompositeTransform'>CompositeTransform</option><option value='TransitionRuleTransform'>TransitionRuleTransform</option><option value='GrowthCurveTransform'>GrowthCurveTransform</option></select></div></div><br>"+
-   "<div class='row'><div class='col-25'><label>library:</label></div><div class='col-75'><input type='text' name='library'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>provider:</label></div><div class='col-75'><input type='text' name='provider'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>data_id:</label></div><div class='col-75'><input type='text' name='data_id'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>queryString:</label></div><div class='col-75'><input type='text' name='queryString'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>provider:</label></div><div class='col-75'><input type='text' name='provider'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>allow_nulls:</label></div><div class='col-75'><input type='checkbox' name='allow_nulls'></div></div><br>"+
-   "<div class='row'><div class='col-25'><label>type:</label></div><div class='col-75'><input type='text' name='type'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>vars(enter elements of the array in comma seperated manner):</label></div><div class='col-75'><input type='text' name='vars'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
-   "<div class='row'><div class='col-25'><label>classifier_set_var:</label></div><div class='col-75'><input type='text' name='classifier_set_var'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div></div><br><br>"+
+   "<div class='row'><div class='col-25'><label>Type of transform:</label></div><div class='col-75'><select id='transforms"+i+"' onchange='change_transform("+i+")'><option value='selecttransform'>Select a transform!</option><option value='LocationIdxFromFlintDataTransform'>LocationIdxFromFlintDataTransform</option><option value='SQLQueryTransform'>SQLQueryTransform</option><option value='CompositeTransform'>CompositeTransform</option><option value='TransitionRuleTransform'>TransitionRuleTransform</option><option value='GrowthCurveTransform'>GrowthCurveTransform</option></select></div></div><br>"+
+   "<div id='transf"+i+"'>"+
+   "</div></div><br><br>"+
    "</div><br>";
+   //adding event listeners to dropdown menu 
+//    var ip=document.getElementById('editor').getElementsByTagName("select");
+//    for(i=0;i<ip.length;i++)
+//    ip[i].addEventListener('change',change_transfom);
 }
 
+//JSON generation from form
 json={},variables={};
-// console.log("a,v,c,d".split(",",4));
+
 function saveFile() {
     var inps=document.getElementsByTagName("input");
     for(i=1;i<Math.max(inps.length,10);i++)
@@ -93,7 +93,44 @@ function writeFile(fileName){
 })
 }
 
-
+function change_transform(i)
+{
+    console.log(document.getElementById("transforms"+i).value);
+    if(document.getElementById("transforms"+i).value=="selecttransform")
+    return;
+    else
+    {
+        if(document.getElementById("transforms"+i).value=="LocationIdxFromFlintDataTransform")
+        {
+            document.getElementById("transf"+i).innerHTML="<div class='row'><div class='col-25'><label>library:</label></div><div class='col-75'><input type='text' name='library'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>provider:</label></div><div class='col-75'><input type='text' name='provider'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>data_id:</label></div><div class='col-75'><input type='text' name='data_id'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>";
+        }
+        else if(document.getElementById("transforms"+i).value=="SQLQueryTransform")
+        {
+            document.getElementById("transf"+i).innerHTML="<div class='row'><div class='col-25'><label>queryString:</label></div><div class='col-75'><input type='text' name='queryString'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>library:</label></div><div class='col-75'><input type='text' name='library'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>provider:</label></div><div class='col-75'><input type='text' name='provider'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>";
+        }
+        else if(document.getElementById("transforms"+i).value=="CompositeTransform")
+        {
+            document.getElementById("transf"+i).innerHTML="<div class='row'><div class='col-25'><label>allow_nulls:</label></div><div class='col-75'><input type='checkbox' name='allow_nulls'></div></div><br>"+
+            "<div class='row'><div class='col-25'><label>library:</label></div><div class='col-75'><input type='text' name='library'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>vars(enter elements of the array in comma seperated manner):</label></div><div class='col-75'><input type='text' name='vars'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>";
+        }
+        else if(document.getElementById("transforms"+i).value=="GrowthCurveTransform")
+        {
+            document.getElementById("transf"+i).innerHTML="<div class='row'><div class='col-25'><label>classifier_set_var:</label></div><div class='col-75'><input type='text' name='classifier_set_var'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div></div></div><br><br>"+
+            "<div class='row'><div class='col-25'><label>library:</label></div><div class='col-75'><input type='text' name='library'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>provider:</label></div><div class='col-75'><input type='text' name='provider'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>";
+        }
+        else if(document.getElementById("transforms"+i).value=="TransitionRuleTransform")
+        {
+            document.getElementById("transf"+i).innerHTML="<div class='row'><div class='col-25'><label>classifier_set_var:</label></div><div class='col-75'><input type='text' name='classifier_set_var'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div></div></div><br><br>"+
+            "<div class='row'><div class='col-25'><label>library:</label></div><div class='col-75'><input type='text' name='library'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>"+
+            "<div class='row'><div class='col-25'><label>provider:</label></div><div class='col-75'><input type='text' name='provider'></div><i class='fa fa-copy' style='font-size:24px'></i>  <i class='fa fa-paste' style='font-size:24px'></i></div><br>";
+        }
+    }
+}
 document.getElementById("object").addEventListener('click',add_object);
 document.getElementById('btnSaveFile').addEventListener('click', saveFile);
-
